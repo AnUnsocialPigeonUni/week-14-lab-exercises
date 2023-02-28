c77e304e1d6d730c01b19f677fcf1f6f3fcd9ddd
Pigeon Uni <jake.turrell@warwick.ac.uk>
2023-02-28 09:44:33 +0000

    updating to current attempt

--- Deploy/Deploy2/setup2.sh
+++ Deploy/Deploy2/setup2.sh
@@ -7,14 +7,21 @@ if [[ -z "${STUDENT_NAME}" ]]; then
   echo "Please run the command \"execute STUDENT_NAME={desired name}\""
   exit 1
 fi
+if [! aws sts get-caller-identity]; then
+    echo >&2 "aws creds not working"
+    exit 2
+fi
+
 
 readonly STACK_NAME="${STUDENT_NAME}-networking"
 readonly TEMPLATE_FILE="$(dirname "${BASH_SOURCE[0]}")/templates/networking.yml"
 readonly AWS_DEFAULT_REGION="eu-west-2"
+readonly UserDataScript=$("${FILE_DIRECTORY}/ec2-setup.sh")
 
-echo $NETWORKING_PARAMS_FILE_DIR
+echo "#############################"
+echo "#    Starting VPC Setup     #"
+echo "#############################"
 
-echo "cloud Formation Deploying"
 aws cloudformation deploy \
   --stack-name "${STACK_NAME}" \
   --template-file "${TEMPLATE_FILE}" \
@@ -23,8 +30,10 @@ aws cloudformation deploy \
   --tags "Project=Confuzzled" "Environment=Dev" "StudentName=${STUDENT_NAME}" "JakeTearDown=True" \
   --region "${AWS_DEFAULT_REGION}" \
   --parameter-overrides \
-      "VpcCidr=10.0.0.0/24" \
-      "PubSubnetACidr=10.0.0.0/26" \
-      "StudentName=JAKE" 
-
-echo "cloud Formation Deploying 2"
+     VpcCidr="10.0.0.0/24" \
+      PubSubnetACidr="10.0.0.0/26" \
+      UserDataScript="${UserDataScript}" \
+      StudentName="JAKE" \
+  --debug &&
+      echo "Complete!" &&
+      exit 1
